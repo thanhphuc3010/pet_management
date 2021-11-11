f5cf5ddf7834fdc368198385663ceb633b593888
phucpt <ptp60.htttql.neu@gmail.com>
2021-11-12 00:16:37 +0700

    Add project files.

---
+++ pet_management/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace pet_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDemo = new System.Windows.Forms.DataGridView();
+            this.Insert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDemo
+            // 
+            this.dgvDemo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDemo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvDemo.Location = new System.Drawing.Point(0, 74);
+            this.dgvDemo.Name = "dgvDemo";
+            this.dgvDemo.RowHeadersWidth = 51;
+            this.dgvDemo.RowTemplate.Height = 24;
+            this.dgvDemo.Size = new System.Drawing.Size(728, 359);
+            this.dgvDemo.TabIndex = 0;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(54, 31);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 1;
+            this.Insert.Text = "button1";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 433);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.dgvDemo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDemo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDemo;
+        private System.Windows.Forms.Button Insert;
+    }
+}
+
---
+++ pet_management/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pet_management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT * FROM staff";
+            dgvDemo.DataSource = DataProvider.Instance.excuteQuery(query);
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            string insert = "INSERT INTO `staff` (`staffNumber`, `fullName`, `birthday`, `email`, `address`, `city`, `country`, `phone`) " +
+                "VALUES ('SD0912', 'Nguyễn Thị Hoàn', '2000-10-30', 'acekembbc@gmail.com', 'Kim Sơn', 'Ninh Bình', 'Việt Nam', '0918848155')";
+            DataProvider.Instance.excuteNonQuery(insert);
+            LoadData();
+        }
+    }
+}
